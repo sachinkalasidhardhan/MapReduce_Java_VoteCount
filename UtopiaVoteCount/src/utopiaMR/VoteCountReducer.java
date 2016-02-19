3764fcb4c169493f68fad0a40c3d5ecf4e4da647
Sachin Kala Sidhardhan <sachin.sidhardhan@outlook.com>
2016-02-19 14:03:10 -0500

    Initial Checkin

---
+++ UtopiaVoteCount/src/utopiaMR/VoteCountReducer.java
@@ -0,0 +1,20 @@
+package utopiaMR;
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class VoteCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context output)
+            throws IOException, InterruptedException {
+        int voteCount = 0;
+        for(IntWritable value: values){
+            voteCount+= value.get();
+        }
+        output.write(key, new IntWritable(voteCount));
+    }
+}
